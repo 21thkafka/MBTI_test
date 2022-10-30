0dc3dca12d6632c425cfc71d52477ec88a74cf08
caudeutsch@gmail.com <82437848+21thkafka@users.noreply.github.com>
2022-10-30 18:52:51 +0900

    카카오 공유하기 js 작업

--- js/result.js
+++ js/result.js
@@ -8,6 +8,46 @@ $(function(){
     $("meta[property='og\\:image']").attr('content', img)
 });
 
+Kakao.init('a9c0b15a4cbe5cdd68fae77c13c3b789');
+Kakao.isInitialized();
+
+function sendLink() {
+    let result_url = window.location.href;
+    Kakao.Link.sendDefault({
+        objectType: 'feed',
+        content: {
+            title: '나의 개발 유형은?',
+            description: '나에게 꼭 맞는 개발 유형을 알아보자!!',
+            imageUrl: 'https://mbit.weniv.co.kr/static/img/mbit_thumbnail.png',
+            link: {
+                mobileWebUrl: 'https://mbit.weniv.co.kr',
+                webUrl: 'https://mbit.weniv.co.kr',
+            },
+        },
+        social: {
+            likeCount: 286,
+            commentCount: 45,
+            sharedCount: 845,
+        },
+        buttons: [
+        {
+            title: '결과 보러가기',
+            link: {
+                webUrl: result_url,
+                mobileWebUrl: result_url,
+            },
+        },
+        {
+            title: '테스트 하러가기',
+            link: {
+                webUrl: 'https://mbit.weniv.co.kr',
+                mobileWebUrl: 'https://mbit.weniv.co.kr',
+            },
+        },
+        ],
+    });
+}
+
 function copyUrl(){
     let tmp = document.createElement('input');
     let url = location.href;
@@ -28,5 +68,8 @@ function sharefacebook(){
     window.open(link);
 }
 
+
 copyBtn.addEventListener('click', copyUrl);
 facebookShare.addEventListener('click', sharefacebook);
+
+kakaoShare.addEventListener('click', sendLink);
